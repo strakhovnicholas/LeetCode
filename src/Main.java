d6f222e13577d568882b057d981fbe98c501b47d
Strakhov Nicholas <strahovnikolaj27@gmail.com>
2025-03-25 13:14:43 +0300

    Новая задача

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
+import Top_Interview_150.Array_String.ProductExceptSelf;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        int [] arr = {0,1,3,1,2,5};
+        int[] newArr = ProductExceptSelf.productExceptSelf(arr);
+/*        for (int i = 0; i < newArr.length; i++) {
+            System.out.print(newArr[i] + " ");
+        }*/
     }
 }
--- src/Top_Interview_150/Array_String/ProductExceptSelf.java
+++ src/Top_Interview_150/Array_String/ProductExceptSelf.java
@@ -1,4 +1,27 @@
 package Top_Interview_150.Array_String;
 
+import java.util.Arrays;
+
 public class ProductExceptSelf {
+    public static int[] productExceptSelf(int[] nums) {
+        int[] output = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            output[i] = 1;
+        }
+
+        int left = 1;
+        for (int i = 0; i < nums.length; i++) {
+            output[i] *= left;
+            left *= nums[i];
+        }
+
+        System.out.println();
+        int right = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            output[i] *= right;
+            right *= nums[i];
+        }
+
+        return output;
+    }
 }
